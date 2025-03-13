536a85e60de00dc3227844b12b683777dba7307c
NerfedDoge <nguyenhoangnhat5atv@gmail.com>
2025-03-13 22:18:35 +0700

    refactor: move model architecture to a separate file for cleanliness

---
+++ model_architecture.py
@@ -0,0 +1,66 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=stride, padding=1
+        )
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1
+        )
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
+        # self.dropout = nn.Dropout(p=0.2)
+
+        # Skip connections (Identity mapping)
+        self.skip = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(num_features=out_channels),
+            )
+
+    def forward(self, x):
+        identity = self.skip(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+        # x = self.dropout(x)
+        x += identity
+        return F.relu(x)
+
+
+class CustomResnet(nn.Module):
+    def __init__(self, num_classes=7):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=32)
+        self.layer1 = self._make_layer(32, 64, 2, stride=2)
+        self.layer2 = self._make_layer(64, 128, 2, stride=2)
+        self.layer3 = self._make_layer(128, 256, 2, stride=2)
+        self.layer4 = self._make_layer(256, 512, 2, stride=2)
+
+        self.pool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+        # self.dropout = nn.Dropout(p=0.3)
+
+    def _make_layer(self, in_channels, out_channels, num_blocks, stride):
+        layers = [ResidualBlock(in_channels, out_channels, stride)]
+        for _ in range(1, num_blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        # x = self.dropout(x)
+        return self.fc(x)
